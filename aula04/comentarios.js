6f2df93e7dd44f539ef1fa8818053492b9c344d2
Paulo Ricardo <pauloricardo.network@gmail.com>
2026-08-24 21:37:12 -0300

    Primeiro commit

---
+++ aula04/comentarios.js
@@ -0,0 +1,13 @@
+// Escreva um comentário de uma linha
+console.log("Este é um comentário de uma linha"); // Aqui está outro comentário de uma linha
+console.log("Este é um comentário de uma linha também"); // Comentário adicional
+console.log("Este é um comentário de uma linha final"); // Último comentário de uma linha
+//Vai ser ignorado pelo interpretador
+
+/* 
+Esse é um comentário de múltiplas linhas
+Ele pode se estender por várias linhas
+E é útil para explicar trechos de código mais complexos
+*/
+
+
