923ee0760ed03a3bab33ffb45d18b201103fe20d
Paulo Ricardo <pauloricardo.network@gmail.com>
2026-08-30 13:16:29 -0300

    Constantes com const

---
+++ aula07/index.js
@@ -0,0 +1,20 @@
+//Não podemos criar constantes com palavras reservadas;
+//Constantes precisam ter nomes significativos;
+//Não podemos começar o nome de uma constante com um número;
+//Não podem conter espaços ou traços;
+//Utilizamos camelCase para nomear constantes;
+//Case-sensitive;
+//Não podemos redeclarar constantes com const;
+//Não utilize var, utilize let ou const.
+
+
+//String = Text | Number = Numero
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+const resultadoDuplicado = resultado * 2;
+
+
+
+
+
