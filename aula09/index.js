a8fe8d665fea6302aa811a118853e6d2729e8e98
Paulo Ricardo <pauloricardo.network@gmail.com>
2026-08-30 15:54:53 -0300

    Let vs Var - Primeira diferença

---
+++ aula09/index.js
@@ -0,0 +1,13 @@
+var nome = 'Paulo';
+var nome = 'Ricardo';
+
+console.log(nome); // Ricardo
+
+/*
+A principal diferença entre var e let
+é o escopo, ou seja, onde a variável pode 
+ser acessada. E também, a variável declarada com 
+var pode ser redeclarada e atualizada, enquanto a
+ variável declarada com let pode ser atualizada, 
+ mas não redeclarada.
+*/
