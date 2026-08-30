0949975a2f179bec0e268b35517f0d723be65074
Paulo Ricardo <pauloricardo.network@gmail.com>
2026-08-30 13:01:34 -0300

    Variáveis com let

---
+++ aula06/index.js
@@ -0,0 +1,18 @@
+let nome = 'Paulo'; // String
+
+console.log(nome, 'nasceu em 1984');
+console.log('Em 2000, ', nome, ' conheceu Maria');
+console.log(nome, 'casou-se com Maria em 2012');
+console.log('Maria teve 1 filho com ' + nome + ' em 2015');
+console.log('O filho de ' + nome + ', se chama Eduardo');
+
+
+//Não podemos criar variáveis com palavras reservadas;
+//Variaveis precisam ter nomes significativos;
+//Não podemos começar o nome de uma variável com um número;
+//Não podem conter espaços ou traços;
+//Utilizamos camelCase para nomear variáveis;
+//Case-sensitive;
+//Não podemos redeclarar variáveis com let;
+//Não utilize var, utilize let.
+
