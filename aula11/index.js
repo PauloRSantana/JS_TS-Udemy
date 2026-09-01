86b4a50099458145a95295bcf6aaded9539eb992
Paulo Ricardo <pauloricardo.network@gmail.com>
2026-08-31 22:49:22 -0300

    Operadores aritméticos, de atribuição e incremento

---
+++ aula11/index.js
@@ -0,0 +1,13 @@
+/**
+ * Arithmetic Operations
+ * + Addition / Concatenation (-/*)
+ * ** Potentiation
+ *  % Rest of Division
+ */ 
+
+const num1 = 10;
+const num2 = 5;
+const num3 = 12;
+console.log(num1 + num2 * num3); // 70
+console.log(num1 ** num2); // 100000
+console.log(num3 % num2); // 2    
